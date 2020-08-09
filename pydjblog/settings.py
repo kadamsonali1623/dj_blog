c6c13b279b536ef3bd8f7f5945e4f79ad1d498a7
sonali <sonali22.kadam@gmail.com>
2020-08-09 11:54:45 +0530

    updated settings.py with ALLOWED_HOSTS

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n2c0i!=!aa)kbh0@!ignlzfe=c*@zyo_tpq9(3j72-d7o)+4ro'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sonalipydjblog.herokuapp.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
